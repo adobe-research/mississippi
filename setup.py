11f72d9cc2a31ede1603ade380cd9cbf454481d4
lipka <lipka@adobe.com>
2013-08-06 16:56:54 -0700

    call sudo python setup.py develop

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mississippi',
+      version='0.1',
+      description='Simple batch processing with AWS resources.',
+      url='',
+      author='Nedim Lipka',
+      author_email='lipka@adobe.com',
+      license='',
+      packages=['ms', 'examples'],
+      zip_safe=False)
